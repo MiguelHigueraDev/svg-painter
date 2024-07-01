a4dcc38cb12ea34f1fcec447a0bb1995b5151645
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-06-30 22:10:18 -0400

    add credits

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import SVGInput from './components/SVGInput';
 import SVGRender from './components/SVGRender';
 
 function App() {
-  const [svgInput, setSvgInput] = useState("");
+  const [svgInput, setSvgInput] = useState('');
 
   const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setSvgInput(e.target.value);
@@ -18,18 +18,32 @@ function App() {
   };
 
   const handleClearInput = () => {
-    setSvgInput("");
+    setSvgInput('');
   };
 
   return (
     <div className="flex gap-10 p-4 h-screen w-screen items-center justify-center">
       <div className="w-1/3 flex flex-col gap-14 max-w-[600px]">
-        <SVGInput svgInput={svgInput} onChange={handleInputChange} onClear={handleClearInput} />
+        <SVGInput
+          svgInput={svgInput}
+          onChange={handleInputChange}
+          onClear={handleClearInput}
+        />
         <ColorList svgInput={svgInput} onChange={handleColorChange} />
       </div>
       <div className="w-1/3">
         <SVGRender svgInput={svgInput} />
       </div>
+      <p className="fixed bottom-3 left-3">
+        Made by{' '}
+        <a
+          className="hover:text-purple-400"
+          target="_blank"
+          href="https://github.com/miguelhigueradev"
+        >
+          MiguelHigueraDev
+        </a>
+      </p>
     </div>
   );
 }
